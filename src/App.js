85909373fb7dc80efe19e29ebf0f2dfa25eabf09
William Sulinski <w@williamsulinski.com>
2020-01-06 13:50:10 -0800

    Add ClassList, ClassDetails, CreateClass, InstructorDashboard, PassDetails, PassList

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import Classes from './components/Classes'
+
+import InstructorDashboard from './components/instructor/InstructorDashboard';
+import CreateClass from './components/instructor/CreateClass'
 
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <h1>Anywhere Fitness</h1>
-
-      </header>
-      <Route exact path='/' component={Classes} />
+      </header> */}
+      <Route exact path='/' component={InstructorDashboard} />
+      <Route path='/instructor/createclass' component={CreateClass} />
     </div>
   );
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,40 @@
 const initialState = {
     classes: [
         {
+            className: 'CrossFit Monday',
+            type: 'CrossFit',
+            startTime: '11:45PM',
+            durationMinutes: 45,
+            intensity: 'high',
+            location: 'Studio Los Feliz',
+            maxClassSize: 10,
+            clients: [
+                'susieclient'
+            ],
             date: '2019-12-12',
-            className: 'CrossFit'
+            instructor: 'joeinstructor',
+        },
+        {
+            className: 'Yoga Tues',
+            type: 'Yoga',
+            startTime: '12:45PM',
+            durationMinutes: 30,
+            intensity: 'low',
+            location: 'Studio Los Feliz',
+            maxClassSize: 5,
+            clients: [
+                'susieclient'
+            ],
+            date: '2019-12-12',
+            instructor: 'joeinstructor',
+        }
+    ],
+    passes: [
+        {
+            className: 'CrossFit',
+            instructor: 'joeinstructor',
+            client: 'susieclient',
+            classesRemaining: 5
         }
     ]
 }
