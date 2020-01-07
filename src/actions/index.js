5b2edb75d296ca7770f2042b4845f567b588be79
Bryan <17adamsbryk@washk12.org>
2020-01-06 22:16:03 -0700

    Refactored more state into redux and actions, got adding and removing of classes functionality

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,19 @@
 export const ADD_CLASS = 'ADD_CLASS';
+export const ADDTO_CLASS = 'ADDTO_CLASS';
+export const SCHEDULE_CLASS = 'SCHEDULE_CLASS';
+export const UNSCHEDULE_CLASS = 'UNSCHEDULE_CLASS';
+
 
 export const addClass = newClass => {
     return { type: ADD_CLASS, payload: newClass }
+}
+
+export const addToClass = addToClass => {
+    return {type: ADDTO_CLASS, payload: addToClass}
+}
+export const scheduleClass = scheduleClass => {
+    return {type: SCHEDULE_CLASS, payload: scheduleClass}
+}
+export const unscheduleClass = unscheduleClass => {
+    return {type: UNSCHEDULE_CLASS, payload: unscheduleClass}
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,59 +1,67 @@
-import { ADD_CLASS } from '../actions/index'
+import { ADD_CLASS, SCHEDULE_CLASS, UNSCHEDULE_CLASS } from "../actions/index";
 
 const initialState = {
-    classes: [
-        {
-            className: 'CrossFit Monday',
-            classType: 'CrossFit',
-            startTime: '12:45',
-            durationMinutes: 45,
-            intensity: 'high',
-            location: 'Studio Los Feliz',
-            maxClassSize: 10,
-            clients: [
-                'susieclient'
-            ],
-            date: '2019-12-12',
-            instructor: 'joeinstructor',
-            id: null,
-        },
-        {
-            className: 'Yoga Tues',
-            classType: 'Yoga',
-            startTime: '12:45',
-            durationMinutes: 30,
-            intensity: 'low',
-            location: 'Studio Los Feliz',
-            maxClassSize: 5,
-            clients: [
-                'susieclient'
-            ],
-            date: '2019-12-12',
-            instructor: 'joeinstructor',
-        }
-    ],
-    passes: [
-        {
-            className: 'CrossFit',
-            instructor: 'joeinstructor',
-            client: 'susieclient',
-            classesRemaining: 5
-        }
-    ]
-}
+  classes: [
+    {
+      className: "CrossFit Monday",
+      classType: "CrossFit",
+      startTime: "12:45",
+      durationMinutes: 45,
+      intensity: "high",
+      location: "Studio Los Feliz",
+      maxClassSize: 10,
+      clients: ["susieclient"],
+      date: "2019-12-12",
+      instructor: "joeinstructor",
+      id: null,
+      scheduled: false
+    },
+    {
+      className: "Yoga Tues",
+      classType: "Yoga",
+      startTime: "12:45",
+      durationMinutes: 30,
+      intensity: "low",
+      location: "Studio Los Feliz",
+      maxClassSize: 5,
+      clients: ["susieclient"],
+      date: "2019-12-12",
+      instructor: "joeinstructor",
+      scheduled: false
+    }
+  ],
+  passes: [
+    {
+      className: "CrossFit",
+      instructor: "joeinstructor",
+      client: "susieclient",
+      classesRemaining: 5
+    }
+  ],
+  scheduledClasses: [],
+};
 
 export const classReducer = (state = initialState, action) => {
-    console.log(state, action);
-    switch (action.type) {
-        case ADD_CLASS:
-            return {
-                ...state,
-                classes: [
-                    ...state.classes,
-                    action.payload
-                ]
-            };
-        default:
-            return state
-    }
-}
+  console.log(state, action);
+  switch (action.type) {
+    case ADD_CLASS:
+      return {
+        ...state,
+        classes: [...state.classes, action.payload]
+      };
+    case SCHEDULE_CLASS:
+      return {
+        ...state,
+        scheduledClasses: [...state.scheduledClasses, action.payload]
+      };
+      case UNSCHEDULE_CLASS:
+      return {
+        ...state,
+        scheduledClasses: [...state.scheduledClasses.filter((item, index) => {
+            return item != action.payload
+        })]
+      };
+    default:
+      return state;
+  }
+};
