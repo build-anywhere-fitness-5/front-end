4ed486e78810759d7332d0fc0b95d27678134e64
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-01-08 12:16:54 -0800

    removed Dashboard import from App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { Route } from "react-router-dom";
 
-import Dashboard from "./components/Dashboard";
 import ClientLogin from "./components/Clients/ClientLogin";
 import ClientHome from "./components/Clients/ClientHome";
 
